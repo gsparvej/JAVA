3258e0a73a2b4a5e729f13c5b27abfc5d82da60c
gsparvej <parvejmd4469@gmail.com>
2025-03-24 14:36:06 +0600

    add

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -9,11 +9,22 @@ public class JavaApplication1 {
         
         String name="parvej";
         byte id= 120;
+        char latter='\u0041';
+        boolean status=true;
+        short roll=12952;
+        int number=659565599;
+        float price=15205.75f;
+      
         
         
         
+        System.out.println("This is  " +latter);
         System.out.println("Parvej");
         System.out.println(name.charAt(1));
+        System.out.println(status);
+        System.out.println(roll);
+        System.out.println(number);
+        System.out.println(price);
     }
     
 }
