620d72019a6a42082b7d8bd72dfe994eb636cde4
gsparvej <parvejmd4469@gmail.com>
2025-03-31 15:41:06 +0600

    add

--- CurrentTime/src/currenttime/CurrentTime.java
+++ CurrentTime/src/currenttime/CurrentTime.java
@@ -6,7 +6,16 @@ public class CurrentTime {
 
     
     public static void main(String[] args) {
-        long 
+        long Totalmiliseconds=System.currentTimeMillis();
+        System.out.println(Totalmiliseconds+ "");
+        long TotalSeconds=Totalmiliseconds/1000;
+        System.out.println(TotalSeconds+"");
+        long currentSeconds=TotalSeconds%60;
+        long TotalMinutes=TotalSeconds/60;
+        long CurrentMinutes=TotalMinutes%60;
+        long TotalHour=TotalMinutes/60;
+        long CurrentHour=(TotalHour%24);
+        System.out.println(Totalmiliseconds+" Miliseconds is "+CurrentHour+" : "+CurrentMinutes+" : "+currentSeconds+" : ");
     }
     
 }
