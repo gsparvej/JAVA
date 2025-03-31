269573074184ee6527ab7294063e0c643518ff27
gsparvej <parvejmd4469@gmail.com>
2025-03-31 13:31:11 +0600

    update

--- ConvertMinute/src/convertminute/ConvertMinute.java
+++ ConvertMinute/src/convertminute/ConvertMinute.java
@@ -8,25 +8,26 @@ public class ConvertMinute {
 
     
     public static void main(String[] args) {
-//        Scanner value=new Scanner(System.in);
-//        int num1=value.nextInt();
-//        int result1=num1/60;
-//        int result2=result1/60;
-//        int result3=result2%60;
-//        System.out.println(result1+"hour");
-
-
-
-Scanner scanner=new Scanner(System.in);
+        Scanner value=new Scanner(System.in);
         System.out.println("Enter Seconds");
+       int num1=value.nextInt();
+        int minutes=num1/60; // minutes
+       int remainingSeconds=num1%60; // seconds 
         
-        int totalSeconds=scanner.nextInt();
-        int hours=totalSeconds/3600; 
-        int remainSeconds=totalSeconds%3600;
-        int minutes=remainSeconds/60;
-        int seconds=remainSeconds%60;
-       
-        System.out.println(hours+"hours,"+minutes+"minutes,"+seconds+"seconds");
+        System.out.println(num1+" Seconds is " +minutes+ " minutes " +remainingSeconds+" seconds ");
+
+
+
+//Scanner scanner=new Scanner(System.in);
+//        System.out.println("Enter Seconds");
+//        
+//        int totalSeconds=scanner.nextInt();
+//        int hours=totalSeconds/3600; 
+//        int remainSeconds=totalSeconds%3600;
+//        int minutes=remainSeconds/60;
+//        int seconds=remainSeconds%60;
+//       
+//        System.out.println(hours+"hours,"+minutes+"minutes,"+seconds+"seconds");
         
     }
     
