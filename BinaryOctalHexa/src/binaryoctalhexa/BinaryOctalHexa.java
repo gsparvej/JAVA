585f65b36a1c714d10e25e7a2eebfdc01b83f643
gsparvej <parvejmd4469@gmail.com>
2025-04-06 00:20:57 +0600

    changed

--- BinaryOctalHexa/src/binaryoctalhexa/BinaryOctalHexa.java
+++ BinaryOctalHexa/src/binaryoctalhexa/BinaryOctalHexa.java
@@ -8,7 +8,7 @@ public class BinaryOctalHexa {
     public static void main(String[] args) {
         System.out.println(0b0011);
         System.out.println(0267343);
-        System.out.println(0x267343);
+        System.out.println(0x2673FA43);
     }
     
 }
