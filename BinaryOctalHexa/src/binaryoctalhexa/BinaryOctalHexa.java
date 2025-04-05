2d5809f698acb335334f2094d8758bbf69b07018
gsparvej <parvejmd4469@gmail.com>
2025-04-06 00:19:18 +0600

    new

---
+++ BinaryOctalHexa/src/binaryoctalhexa/BinaryOctalHexa.java
@@ -0,0 +1,14 @@
+
+package binaryoctalhexa;
+
+
+public class BinaryOctalHexa {
+
+    
+    public static void main(String[] args) {
+        System.out.println(0b0011);
+        System.out.println(0267343);
+        System.out.println(0x267343);
+    }
+    
+}
