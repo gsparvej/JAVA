969d64d06207828d209a56334c71d05c70afaf82
gsparvej <parvejmd4469@gmail.com>
2025-04-05 11:28:33 +0600

    update

--- PracticeHome/src/practicehome/PracticeHome.java
+++ PracticeHome/src/practicehome/PracticeHome.java
@@ -21,15 +21,32 @@ public class PracticeHome {
 //        System.out.println(a);
 
 
-        Scanner price= new Scanner(System.in);
-        System.out.println("Enter Product Price");
-        double PurchaseAmount=price.nextDouble();
-        Scanner taxes= new Scanner(System.in);
-        System.out.println("Enter Product Price taxes");
-         double tax=taxes.nextDouble();
-        double pay=PurchaseAmount*(tax/100);
-        double result=(PurchaseAmount+pay)/100;
-        System.out.println( "$ "+result+" You Must Pay For This Product ");
+//        Scanner price= new Scanner(System.in);
+//        System.out.println("Enter Product Price");
+//        double PurchaseAmount=price.nextDouble();
+//        Scanner taxes= new Scanner(System.in);
+//        System.out.println("Enter Product Price taxes");
+//         double tax=taxes.nextDouble();
+//        double pay=PurchaseAmount*(tax/100);
+//        double result=(PurchaseAmount+pay)/100;
+//        System.out.println( "$ "+result+" You Must Pay For This Product ");
+
+
+//        Scanner val=new Scanner(System.in);
+//        System.out.println("Enter KM value");
+//        float input=val.nextFloat();
+//        float result=input*1000/3600;
+//        System.out.println("Your m/s result is "+result+"m/s ");
+
+        
+           Scanner minutes=new Scanner(System.in);
+           System.out.println("Enter Your Minutes");
+           double input=minutes.nextDouble();
+           int year=(int)input/518400;
+           int day=(int)input%518400;
+           int CurrentDays=day/1440;
+           System.out.println(year+" Years "+CurrentDays+" Days ");
+           System.out.println(day);
         
         
     }
