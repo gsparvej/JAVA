d1b6e80d8a688d3e64b47146bad5448a15dc31c4
gsparvej <parvejmd4469@gmail.com>
2025-04-06 23:44:24 +0600

    add

--- PositiveNegativeZero/src/positivenegativezero/PositiveNegativeZero.java
+++ PositiveNegativeZero/src/positivenegativezero/PositiveNegativeZero.java
@@ -1,12 +1,30 @@
 
 package positivenegativezero;
 
+import java.util.Scanner;
+
 
 public class PositiveNegativeZero {
 
     
     public static void main(String[] args) {
-       
+        Scanner value=new Scanner(System.in);
+        
+        System.out.println("To Check Its Positive , Negative or Zero   ");
+        System.out.println("Enter A Number : ");
+        
+        int input=value.nextInt();
+        
+        if(input>0){
+            System.out.println(input+" Its a Positive Number ");
+        }
+        else if(input<0){
+            System.out.println(input+" Its a Negative Number ");
+        }
+        else{
+            System.out.println(input+" Its a Big Zero ");
+        }
+               
     }
     
 }
