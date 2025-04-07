fa37516d9897a6673973eea680671fdbd60e1bb0
gsparvej <parvejmd4469@gmail.com>
2025-04-07 10:13:50 +0600

    add

--- GradePoint/src/gradepoint/GradePoint.java
+++ GradePoint/src/gradepoint/GradePoint.java
@@ -12,17 +12,21 @@ public class GradePoint {
         System.out.println("Enter Your Result ");
         int result=r.nextInt();
         
-        if(result>=90){
-            System.out.println("Your Grade is 'A' ");
+        if(result>100){
+            System.out.println("Please Enter Correct Result Below 100 ");
         }
-        else if(result>=89){
-            System.out.println("Your Grade is 'B' ");
+
+        else if(result<=69){
+            System.out.println("Your Grade is 'D' ");
         }
-        else if(result>=79){
+        else if(result<=79){
             System.out.println("Your Grade is 'C' ");
         }
-        else if(result>=69){
-            System.out.println("Your Grade is 'D' ");
+        else if(result<=79){
+            System.out.println("Your Grade is 'B' ");
+        }
+         else if(result<=100){
+            System.out.println("Your Grade is 'A' ");
         }
         
         else {
