95eb3bf69a2340d022bb958dc50e069fa16e30e8
gsparvej <parvejmd4469@gmail.com>
2025-04-07 16:45:24 +0600

    add

--- CalculatorUsingSwitch/src/calculatorusingswitch/CalculatorUsingSwitch.java
+++ CalculatorUsingSwitch/src/calculatorusingswitch/CalculatorUsingSwitch.java
@@ -1,11 +1,45 @@
 
 package calculatorusingswitch;
 
+import java.util.Scanner;
+
 
 public class CalculatorUsingSwitch {
 
     
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        
+        System.out.println(" Enter 1st Number ");
+        double n1=s.nextDouble();
+        
+        System.out.println(" Enter 2nd Number ");
+        double n2=s.nextDouble();
+        
+        System.out.println("Enter your choices like as + - * / ");
+        String sign=s.next();
+        
+        double result=0;
+        
+        
+        switch (sign) {
+            case "+":
+                result=n1+n2;
+                break;
+            case "-":
+                result=n1-n2;
+                break;
+            case "*":
+                result=n1*n2;
+                break;
+            case "/":
+                result=n1/n2;
+                break;
+            default:
+                System.out.println(" Invalid Choices ");
+        }
+        
+        System.out.println(result+"");
         
     }
     
