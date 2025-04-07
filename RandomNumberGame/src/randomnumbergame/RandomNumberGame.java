7d0a93ac9cb78d69bdfe69a89506030f1c6d70c6
gsparvej <parvejmd4469@gmail.com>
2025-04-07 18:22:30 +0600

    add

--- RandomNumberGame/src/randomnumbergame/RandomNumberGame.java
+++ RandomNumberGame/src/randomnumbergame/RandomNumberGame.java
@@ -1,12 +1,27 @@
 
 package randomnumbergame;
 
+import java.util.Scanner;
+
 
 public class RandomNumberGame {
 
    
     public static void main(String[] args) {
+        Scanner number=new Scanner(System.in);
+        System.out.println("Please Guase a Number like 0 to 9 ");
+        int n=number.nextInt();
         
+        int randomNum = (int)(Math.random() * 10);
+        System.out.println(randomNum+"");
+        
+        if(n==randomNum){
+            System.out.println(" Congratulations - You are win ");
+        }
+        
+        else{
+            System.out.println(" SORRY You are Loose");
+        }
     }
     
 }
