e8450f7988cf51214cb6f847c3c10e39b198b947
gsparvej <parvejmd4469@gmail.com>
2025-04-07 11:54:13 +0600

    add

--- SelectTraingle/src/selecttraingle/SelectTraingle.java
+++ SelectTraingle/src/selecttraingle/SelectTraingle.java
@@ -26,6 +26,36 @@ public class SelectTraingle {
         
         System.out.println("Enter Coordinates Of 'C' Point of (y-axis) ");
         int y3=points.nextInt();
+        
+        int result1=x2-x1;
+        int result2=y2-y1;
+        
+        int result3=x3-x2;
+        int result4=y3-y2;
+        
+        int result5=x1-x3;
+        int result6=y1-y3;
+        
+        float distanceAB=(float)Math.sqrt(Math.pow(result1, 2)+Math.pow(result2, 2));
+        
+        float distanceBC=(float)Math.sqrt(Math.pow(result3, 2)+Math.pow(result4, 2));
+        
+        float distanceCA=(float)Math.sqrt(Math.pow(result5, 2)+Math.pow(result6, 2));
+        
+        
+        System.out.println("  AB = " +distanceAB+ " ," + " BC = "+distanceBC+ "   ," + "  CA  = " +distanceCA);
+        
+        
+        
+        if(distanceAB==distanceBC && distanceAB==distanceCA && distanceBC== distanceCA){
+            System.out.println("This is a Equilateral Triangle");
+        }
+        else if(!(distanceAB==distanceBC) && !(distanceAB==distanceCA) && !(distanceBC== distanceCA)){
+            System.out.println(" This is a Scalene Triangle");
+        }
+        else{
+            System.out.println(" This is a Isosceles Triangle ");
+        }
     }
     
 }
