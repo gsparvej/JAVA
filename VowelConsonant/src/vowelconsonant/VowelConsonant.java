1728e73c457095d1464cf309d7e167dc8239dfd6
gsparvej <parvejmd4469@gmail.com>
2025-04-07 10:38:52 +0600

    add

--- VowelConsonant/src/vowelconsonant/VowelConsonant.java
+++ VowelConsonant/src/vowelconsonant/VowelConsonant.java
@@ -10,7 +10,12 @@ public class VowelConsonant {
 
     
     public static void main(String[] args) {
-      
+            Scanner input=new Scanner(System.in);
+            System.out.println("Enter Your Finding Alphabet Like as A or B ");
+            char alphabet=input.next().toLowerCase();
+            
+            if(alphabet='a' ||alphabet='e'){
+            }
     }
     
 }
