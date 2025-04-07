3d768be499e111aacd2221a93ec62f7e5b8b90e5
gsparvej <parvejmd4469@gmail.com>
2025-04-07 11:25:35 +0600

    add

--- SelectTraingle/src/selecttraingle/SelectTraingle.java
+++ SelectTraingle/src/selecttraingle/SelectTraingle.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package selecttraingle;
 
-/**
- *
- * @author MY COMPUTER
- */
+import java.util.Scanner;
+
+
 public class SelectTraingle {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner points=new Scanner(System.in);
+        System.out.println("Enter Coordinates Of 'A' Point of (x-axis) ");
+        int x1=points.nextInt();
+        
+        System.out.println("Enter Coordinates Of 'A' Point of (y-axis) ");
+        int y1=points.nextInt();
+        
+        System.out.println("Enter Coordinates Of 'B' Point of (x-axis) ");
+        int x2=points.nextInt();
+        
+        System.out.println("Enter Coordinates Of 'B' Point of (y-axis) ");
+        int y2=points.nextInt();
+        
+        System.out.println("Enter Coordinates Of 'C' Point of (x-axis) ");
+        int x3=points.nextInt();
+        
+        System.out.println("Enter Coordinates Of 'C' Point of (y-axis) ");
+        int y3=points.nextInt();
     }
     
 }
