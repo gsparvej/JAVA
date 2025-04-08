0e398243d1bb02ae433b60ad2a983df5923aba53
gsparvej <parvejmd4469@gmail.com>
2025-04-08 16:59:26 +0600

    add

--- LearnString/src/learnstring/LearnString.java
+++ LearnString/src/learnstring/LearnString.java
@@ -6,6 +6,11 @@ public class LearnString {
 
     
     public static void main(String[] args) {
+        String message=" Welcome To Java ";
+        System.out.println(" length = "+message.length());
+        System.out.println(" CharAt = "+message.charAt(7));
+        System.out.println(" Concate = "+message.concat( "with parvej"));
+        
         
     }
     
