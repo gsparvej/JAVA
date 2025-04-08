7174259abc57978a0b3360509958a639ac76c272
gsparvej <parvejmd4469@gmail.com>
2025-04-08 18:57:10 +0600

    update

--- ClassTest/src/classtest/ClassTest.java
+++ ClassTest/src/classtest/ClassTest.java
@@ -24,12 +24,66 @@ public class ClassTest {
 
            System.out.println("Enter Year ");
         String year=n.next();
-        System.out.println(" Enter Month ");
-        String month=n.next();
+//        System.out.println(" Enter Month ");
+//        String month=n.next();
         System.out.println(" Enter Date ");
         String date=n.next();
         
-        System.out.println(month.concat( " ")+date.concat(", ")+year.concat(""));
+        
+//        System.out.println(month.concat( " ")+date.concat(", ")+year.concat(""));
+        
+        
+        System.out.println("Enter month like as  1 or 2 or 8 or 12 ");
+        int num=n.nextInt();
+        String result = " ";
+        // System.out.println(month.concat( " ")+date.concat(", ")+year.concat(""));
+        
+        switch (num) {
+            case 1:
+                result="January";
+                break;
+            case 2:
+                result="February";
+                break;
+            case 3:
+                result="March";
+                break;
+            case 4:
+                result="April";
+                break;
+            case 5:
+                result="May";
+                break;
+            case 6:
+                result="June";
+                break;
+            case 7:
+                result="July";
+                break;
+            case 8:
+                result="August";
+                break;
+            case 9:
+                result="September";
+                break;
+            case 10:
+                result="October";
+                break;
+            case 11:
+                result="November";
+                break;
+                case 12:
+                result="December";
+                break;
+            default:
+                System.out.println(" Invalid Identifiers ");
+                
+                
+                
+                
+        }
+        
+        System.out.println(result.concat( " ")+date.concat(", ")+year.concat(""));
         
     }
     
