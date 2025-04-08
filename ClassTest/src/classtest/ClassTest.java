7a5bd011aa88adbdba5d6a94bdc1662424fa08ed
gsparvej <parvejmd4469@gmail.com>
2025-04-08 18:14:03 +0600

    add

---
+++ ClassTest/src/classtest/ClassTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package classtest;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class ClassTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner n=new Scanner(System.in);
+        String name=n.next();
+        String Programmer = "Programmer";
+        System.out.println(name+" "+Programmer);
+    }
+    
+}
