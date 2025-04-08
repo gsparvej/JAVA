189a4216c79c6624e193d42e47a2de47a8d0f908
gsparvej <parvejmd4469@gmail.com>
2025-04-08 18:25:26 +0600

    add

--- ClassTest/src/classtest/ClassTest.java
+++ ClassTest/src/classtest/ClassTest.java
@@ -12,14 +12,25 @@ public class ClassTest {
     
     public static void main(String[] args) {
         Scanner n=new Scanner(System.in);
-        String name=n.next();
-        String Programmer = "Programmer";
+//        String name=n.next();
+//        String Programmer = "Programmer";
+//        
+//        
+//        System.out.println(name+" "+Programmer);
+//        
+//        
+//        System.out.println(name.concat(" Programmer"));
+
+
+           System.out.println("Enter Year ");
+        String year=n.next();
+        System.out.println(" Enter Month ");
+        String month=n.next();
+        System.out.println(" Enter Date ");
+        String date=n.next();
         
+        System.out.println(month.concat( " ")+date.concat(", ")+year.concat(""));
         
-        System.out.println(name+" "+Programmer);
-        
-        
-        System.out.println(name.concat(" Programmer"));
     }
     
 }
