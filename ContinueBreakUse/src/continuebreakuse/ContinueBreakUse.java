767121b304055e3796795263fb50d1fa29f870da
gsparvej <parvejmd4469@gmail.com>
2025-04-09 18:55:12 +0600

    add

---
+++ ContinueBreakUse/src/continuebreakuse/ContinueBreakUse.java
@@ -0,0 +1,24 @@
+
+package continuebreakuse;
+
+
+public class ContinueBreakUse {
+
+    
+    public static void main(String[] args) {
+        int targetValue=50;
+        int count=1;
+        
+        while (targetValue >= count){
+        
+                if(count%3 ==0 && count%5 ==0){
+                count++;
+                continue;
+                }
+                System.out.println(count+ " No. is ");
+        count++;
+        }
+        
+    }
+    
+}
