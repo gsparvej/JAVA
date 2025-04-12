89b3497de1ee8afcb4b172859442a740b1aaa662
gsparvej <parvejmd4469@gmail.com>
2025-04-12 16:13:13 +0600

    changed

--- LearnMethodOverLoading/src/learnmethodoverloading/LearnMethodOverLoading.java
+++ LearnMethodOverLoading/src/learnmethodoverloading/LearnMethodOverLoading.java
@@ -1,12 +1,60 @@
 
 package learnmethodoverloading;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class LearnMethodOverLoading {
 
    
     public static void main(String[] args) {
         
+        
+        Scanner s=new Scanner(System.in);
+        System.out.println(" Enter how many number u want to input ");
+        int n=s.nextInt();
+        
+        int[] myArray=new int[n];
+        for(int i=0;i<n;i++){
+        
+            System.out.println("Enter Numbers ");
+            int userInput=s.nextInt();
+            myArray[i]=userInput;
+        }
+        sum(myArray);
+        
+        
     }
+    
+    
+    public static int sum(){
+    
+    return 10+20;
+    
+    }
+    public static int sum(int a){
+    
+    return a+20;
+    
+    }
+    public static int sum(int a,int b){
+       
+    
+    return a+b;
+    
+    }
+    
+    public static void sum(int...a){
+    
+    int sum=0;
+    for(int item :a){
+        sum+=item;
+    }
+    
+        System.out.println(Arrays.toString(a));
+        System.out.println(sum+"");
+    }
+    
     
 }
