91567739efae9aa5399668fb664968b9ca12fc1c
gsparvej <parvejmd4469@gmail.com>
2025-04-13 08:58:02 +0600

    changed

--- PracticeHomeChatgpt/src/practicehomechatgpt/PracticeHomeChatgpt.java
+++ PracticeHomeChatgpt/src/practicehomechatgpt/PracticeHomeChatgpt.java
@@ -1,11 +1,57 @@
 
 package practicehomechatgpt;
 
+import java.util.Scanner;
+
 
 public class PracticeHomeChatgpt {
 
     
     public static void main(String[] args) {
+        
+        
+//        Scanner s=new Scanner(System.in);
+//        
+//        System.out.println(" How Much You Want ");
+//        int a=s.nextInt();
+//        s.nextLine();
+//        System.out.println(" What do you want to see ");
+//        String text=s.nextLine();
+//       
+//        int i;
+//        int count=0;
+//        
+//        for( i=1;i<=a;i++){
+//            count++;
+//            System.out.println(i+" "+text);
+//        }
+        
+
+            // start******
+
+//        Scanner s=new Scanner(System.in);
+//        
+//        System.out.println(" Enter Number ");
+//        int a=s.nextInt();
+//        
+//        for(int i=1;i<=a;i++){
+//            
+//            if(i%2==0){
+//                System.out.println(i+" Even Number ");
+//            }
+//        }
+            
+
+
+
+        Scanner s=new Scanner(System.in);
+        System.out.println(" Reverse Number ");
+        int a=s.nextInt();
+        for (int i=a;i>=1;i--){
+        
+            System.out.println(i+"");
+        }
+        
         
     }
     
