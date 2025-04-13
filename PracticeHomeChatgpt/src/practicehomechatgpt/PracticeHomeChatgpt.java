db05cca45807d92e7c2d9e36a2f68b0519b72a80
gsparvej <parvejmd4469@gmail.com>
2025-04-13 10:32:11 +0600

    add

--- PracticeHomeChatgpt/src/practicehomechatgpt/PracticeHomeChatgpt.java
+++ PracticeHomeChatgpt/src/practicehomechatgpt/PracticeHomeChatgpt.java
@@ -44,14 +44,76 @@ public class PracticeHomeChatgpt {
 
 
 
-        Scanner s=new Scanner(System.in);
-        System.out.println(" Reverse Number ");
-        int a=s.nextInt();
-        for (int i=a;i>=1;i--){
+//        Scanner s=new Scanner(System.in);
+//        System.out.println(" Reverse Number ");
+//        int a=s.nextInt();
+//        for (int i=a;i>=1;i--){
+//        
+//            System.out.println(i+"");
+//        }
         
-            System.out.println(i+"");
-        }
+
+        // multiplication table start *****
         
+//        Scanner s=new Scanner(System.in);
+//        int a=s.nextInt();
+//       
+//        for(int i=1;i<=10;i++){
+//            System.out.println(a+"*"+i+"="+(a*i));
+//        }
+
+
+
+//        Scanner s=new Scanner(System.in);
+//        System.out.println(" Renge of Sum ");
+//        int a =s.nextInt();
+//        int sum=0;
+//        for(int i=1;i<=a;i++){
+//            sum+=i;
+//            System.out.print(i+"+");
+//        }
+//        System.out.println("="+sum+"");
+
+
+
+
+        // palidrome number test//***** 
+        
+        
+//        Scanner s=new Scanner(System.in);
+//        System.out.println(" Enter Number ");
+//        
+//        int a=s.nextInt();
+//        int digit;
+//        
+//        for(int i=0;i<=a;i++){
+//        digit=a%10;
+//        a=a/10;
+//            System.out.print(digit+"");
+//        }
+        
+
+            
+              Scanner s=new Scanner(System.in);
+              System.out.println(" Enter Name to Palidrome ");
+              String name=s.nextLine();
+              
+              name=name.toLowerCase();
+              String reverse="";
+              
+              for(int i=name.length()-1;i>=0;i--){
+              reverse+=name.charAt(i);
+                  //System.out.print(reverse+"");
+              }
+              System.out.println(reverse+"");
+              
+              if(name.equals(reverse)){
+                  System.out.println(name+" Its Palindrome is "+reverse);
+              }
+              else{
+              System.out.println(name+" Its Not A Palindrome " +reverse);
+              }
+            
         
     }
     
