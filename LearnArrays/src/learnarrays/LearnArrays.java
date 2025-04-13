d6a511965ce998653e21a3f777d4f87fbb2e1c3a
gsparvej <parvejmd4469@gmail.com>
2025-04-13 16:42:18 +0600

    add

--- LearnArrays/src/learnarrays/LearnArrays.java
+++ LearnArrays/src/learnarrays/LearnArrays.java
@@ -1,11 +1,44 @@
 
 package learnarrays;
 
+import java.util.Arrays;
+
 
 public class LearnArrays {
 
     
     public static void main(String[] args) {
+        
+        String[] names=new String[5];
+        int[] marks=new int[5];
+        
+        
+        
+        
+        
+        names[0]="parvej";
+        names[1]="rakib";
+        names[2]="eiasin";
+        names[3]="atik";
+        names[4]="rahmat";
+        
+        
+        marks[0]=65;
+        marks[1]=75;
+        marks[2]=85;
+        marks[3]=80;
+        marks[4]=70;
+        
+        
+        for(int i=0;i<=4;i++){
+        
+            System.out.println(names[i]+" = "+marks[i]);
+            
+        }
+        
+        
+        
+        
         
     }
     
