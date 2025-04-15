eee53890f2ed484472efea8775154a2b3b4d0565
gsparvej <parvejmd4469@gmail.com>
2025-04-15 15:32:31 +0600

    add

--- PracticeArrays/src/practicearrays/PracticeArrays.java
+++ PracticeArrays/src/practicearrays/PracticeArrays.java
@@ -6,6 +6,13 @@ public class PracticeArrays {
 
     public static void main(String[] args) {
        
+        String[] names={"Imran","Masud","Istiaque","Reja","Atik","Eiasin","Rakib"};
+       // String[] passwords={"123","234","345","456","567","678","123"};
+        int[] marks={87,89,56,87,70,90,75};
+        
+        for{int i=0;i<=6;i++}
+        int i = 0;
+        System.out.println("His name is "+names[i]+" got "+marks[i]+" marks ");
     }
     
 }
---
+++ PracticeArrays/src/practicearrays/i.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practicearrays;
+
+/**
+ *
+ * @author MY COMPUTER
+ */
+class i<T0, T1> {
+    
+}
