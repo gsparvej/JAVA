2337e6c8d8788464b9a2efec0dd31df233f0716e
gsparvej <parvejmd4469@gmail.com>
2025-04-16 17:51:21 +0600

    add

--- PracticeSixteenAprilSimpleType/src/practicesixteenaprilsimpletype/PracticeSixteenAprilSimpleType.java
+++ PracticeSixteenAprilSimpleType/src/practicesixteenaprilsimpletype/PracticeSixteenAprilSimpleType.java
@@ -1,12 +1,25 @@
 
 package practicesixteenaprilsimpletype;
 
+import java.util.Scanner;
+
 
 public class PracticeSixteenAprilSimpleType {
 
     
     public static void main(String[] args) {
        
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Total Seconds ");
+        int totalSeconds=s.nextInt();
+        
+        int hour=totalSeconds/3600;
+        int remindSeconds=totalSeconds%3600;
+        int minutes=remindSeconds/60;
+        int seconds=remindSeconds%60;
+        
+        System.out.println(hour+" hours "+minutes+" minutes "+seconds+" seconds ");
+        
     }
     
 }
