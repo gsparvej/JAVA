6d0003c1c1285e0b10286d51d96775b7bb049f9b
gsparvej <parvejmd4469@gmail.com>
2025-04-17 17:04:13 +0600

    add

---
+++ FindMaxMinUsingMethod/src/findmaxminusingmethod/FindMaxMinUsingMethod.java
@@ -0,0 +1,59 @@
+
+package findmaxminusingmethod;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class FindMaxMinUsingMethod {
+
+   
+    public static void main(String[] args) {
+        
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Your Limit ");
+        int limit=s.nextInt();
+        int finale=findMaxMin(limit);
+        System.out.println(finale+"");
+        
+    }
+    
+    
+    public static int findMaxMin(int limit){
+    
+    
+        Scanner s=new Scanner(System.in);
+//        System.out.println("Enter Limit ");
+        int n=s.nextInt();
+        int[] numbers=new int[n];
+        int maxNumber=0;            // sir evabe kore dise ....
+        int minNumber=999999999;
+        int result = 0;
+//        System.out.println(numbers.toString()+" ");
+        
+        for(int i=0;i<n;i++){
+        
+            System.out.println("Enter number "+(i+1));    
+            int userInput=s.nextInt();
+            numbers[i]=userInput;
+        }
+        System.out.println("My Numbers are "+Arrays.toString(numbers));
+        
+        for(int number : numbers){
+        
+            if(number>maxNumber){
+            maxNumber=number;
+            result=maxNumber;
+            }
+            if(number<minNumber){
+            minNumber=number;
+            result=minNumber;
+            }
+        }
+        System.out.println(" Max Number "+maxNumber);
+        System.out.println(" Min Number "+minNumber);
+    
+    return result;
+    }
+    
+}
