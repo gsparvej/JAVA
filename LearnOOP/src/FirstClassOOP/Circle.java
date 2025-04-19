cdb1ed4d0a2ec89b92f014c352142cb80b003763
gsparvej <parvejmd4469@gmail.com>
2025-04-19 19:07:55 +0600

    add

--- LearnOOP/src/FirstClassOOP/Circle.java
+++ LearnOOP/src/FirstClassOOP/Circle.java
@@ -4,4 +4,19 @@ package FirstClassOOP;
 
 public class Circle {
     
+    double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    public double area(){
+    double area=Math.PI*Math.pow(radius, 2);
+    return area;
+    }
+    
+    
 }
--- LearnOOP/src/learnoop/LearnOOP.java
+++ LearnOOP/src/learnoop/LearnOOP.java
@@ -7,6 +7,8 @@ public class LearnOOP {
    
     public static void main(String[] args) {
         
+        System.out.println();
+        
     }
     
 }
