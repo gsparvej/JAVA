bfe10cba2cf421373fef69e21cdbc15d388b0863
gsparvej <parvejmd4469@gmail.com>
2025-04-20 01:03:16 +0600

    add

--- LearnOOP/src/learnoop/LearnOOP.java
+++ LearnOOP/src/learnoop/LearnOOP.java
@@ -2,6 +2,7 @@
 package learnoop;
 
 import FirstClassOOP.Circle;
+import java.util.Scanner;
 
 
 public class LearnOOP {
@@ -15,6 +16,22 @@ public class LearnOOP {
         
         System.out.println(c.perimeter()+" is Perimeter Of The Circle ");
         
+//        Student atik=new Student("Atik ",65);
+//        Student shimul=new Student("Shimul", 70);
+
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter atik's marks ");
+        int atikMarks=s.nextInt();
+        System.out.println("Enter Shimul's marks");
+        int shimulMarks=s.nextInt();
+        
+       
+        Student atik=new Student("Atik");
+        Student shimul=new Student("Shimul");
+        
+        System.out.println(atik.getResult());
+        System.out.println(shimul.getResult());
     }
     
 }
--- LearnOOP/src/learnoop/Student.java
+++ LearnOOP/src/learnoop/Student.java
@@ -3,5 +3,43 @@ package learnoop;
 
 
 public class Student {
+
+    String name;
+    String email;
+    double marks;
+
+    public Student(String atik) {
+    }
+
+    public Student(String name, String email, double marks) {
+        this.name = name;
+        this.email = email;
+        this.marks = marks;
+    }
+
+    public Student(String name, double marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+    
+    public String getResult(){
+    
+    if(marks>=33){
+    
+        return name+" Pass";
+    }
+    else{
+    return name+" Fail";
+    }
+    
+    }
+    public double getMarks(){
+    return marks;
+    }
+    public void setMarks(double marks){
+    
+        this.marks=marks;
+    
+    }
     
 }
