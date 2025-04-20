5884ce3aa9c9e2ed7dfe1103ee179bf2971ee1cc
gsparvej <parvejmd4469@gmail.com>
2025-04-20 23:30:47 +0600

    changed

--- DistanceTwoPointsUsingOOP/src/distancetwopointsusingoop/DistanceTwoPointsUsingOOP.java
+++ DistanceTwoPointsUsingOOP/src/distancetwopointsusingoop/DistanceTwoPointsUsingOOP.java
@@ -1,12 +1,18 @@
 
 package distancetwopointsusingoop;
 
+import maths.Distance;
+
 
 public class DistanceTwoPointsUsingOOP {
 
     
     public static void main(String[] args) {
         
+        
+        Distance d=new Distance(2, 3, 5, 1);
+        
+        System.out.println("Distance is "+d.getDistance());
     }
     
 }
--- DistanceTwoPointsUsingOOP/src/maths/Distance.java
+++ DistanceTwoPointsUsingOOP/src/maths/Distance.java
@@ -4,4 +4,33 @@ package maths;
 
 public class Distance {
     
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+
+    public Distance() {
+    }
+
+    public Distance(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public Distance(int x1, int y1) {
+        this.x1 = x1;
+        this.y1 = y1;
+    }
+    
+    public String getDistance(){
+    
+        int result1=x2-x1;
+        int result2=y2-y1;
+//        String return result1+" Its  "+result2;
+        return result1+" "+result2;
+    
+    }
+    
 }
