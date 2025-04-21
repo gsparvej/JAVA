f8b5b1af71e7b5f80144a71b1cff6d7a4743aa63
gsparvej <parvejmd4469@gmail.com>
2025-04-21 19:07:10 +0600

    changed

--- ClassTestDate21April/src/classtestdate21april/ClassTestDate21April.java
+++ ClassTestDate21April/src/classtestdate21april/ClassTestDate21April.java
@@ -21,24 +21,24 @@ public class ClassTestDate21April {
         
         // 1.
         
-//        System.out.println("enter number limit for fibonacci ");
-//        int n=s.nextInt();
-//        
-//        int num1=0;
-//        int num2=1;
-//        int sum=0;
-//        System.out.println("----------");
-//        System.out.print(num1+" ");
-//        System.out.print(num2+" ");
-//        
-//        for(int i=3;i<=n;i++){
-//        
-//            sum=num1+num2;
-//            num1=num2;
-//            num2=sum;
-//            
-//            System.out.print(""+sum+" ");
-//        }
+        System.out.println("enter number limit for fibonacci ");
+        int n=s.nextInt();
+        
+        int num1=0;
+        int num2=1;
+        int sum=0;
+        System.out.println("----------");
+        System.out.print(num1+" ");
+        System.out.print(num2+" ");
+        
+        for(int i=3;i<=n;i++){
+        
+            sum=num1+num2;
+            num1=num2;
+            num2=sum;
+            
+            System.out.print(""+sum+" ");
+        }
         
     //2. 
     
