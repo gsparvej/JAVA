44998eac2d3bdf078827d1aecd586f93c50da038
gsparvej <parvejmd4469@gmail.com>
2025-04-21 23:35:59 +0600

    changed

--- LearnAccessModifiers/src/learnaccessmodifiers/mobile/AndroidMobile.java
+++ LearnAccessModifiers/src/learnaccessmodifiers/mobile/AndroidMobile.java
@@ -4,5 +4,16 @@ package learnaccessmodifiers.mobile;
 
 public class AndroidMobile {
     
+    String operatingSystem;
+    String store;
+
+    public AndroidMobile() {
+    }
+
+    public AndroidMobile(String operatingSystem, String store) {
+        this.operatingSystem = operatingSystem;
+        this.store = store;
+    }
+    
     
 }
