4000c0c130a43adf8cf86864a42b02bfa85ee0cc
gsparvej <parvejmd4469@gmail.com>
2025-04-21 18:27:48 +0600

    changed

--- ClassTestDate21April/src/classtestdate21april/ClassTestDate21April.java
+++ ClassTestDate21April/src/classtestdate21april/ClassTestDate21April.java
@@ -19,55 +19,105 @@ public class ClassTestDate21April {
         
         Scanner s=new Scanner(System.in);
         
+        // 1.
+        
+//        System.out.println("enter number limit for fibonacci ");
+//        int n=s.nextInt();
+//        
+//        int num1=0;
+//        int num2=1;
+//        int sum=0;
+//        System.out.println("----------");
+//        System.out.print(num1+" ");
+//        System.out.print(num2+" ");
+//        
+//        for(int i=3;i<=n;i++){
+//        
+//            sum=num1+num2;
+//            num1=num2;
+//            num2=sum;
+//            
+//            System.out.print(""+sum+" ");
+//        }
+        
+    //2. 
+    
+//        System.out.println("Enter a number to check its Prime Or Not ");
+//        int n=s.nextInt();
+//        int count=0;
+//        
+//        for(int i=1;i<=n;i++){
+//        
+//            if(n%i==0){
+//            
+//                count++;
+//            }
+//            
+//        }
+//        if(count==2){
+//        
+//            System.out.println("Its Prime Number ");
+//        }
+//        else{
+//        
+//            System.out.println("Its Not Prime Number ");
+//        }
+        
+        
+        
+        
+        
         
         // 3.
-        System.out.println("Enter Five Numbers");
-        int n=s.nextInt();
-        int[] myNumbers=new int[n];
-        
-        int maxNumber=0;
-        int minNumber=999999999;
-        
-        for(int index=0;index<n;index++){
-        
-            System.out.println("Input "+(index+1));
-            int userInput=s.nextInt();
-            myNumbers[index]=userInput;
-        }
-        System.out.println("My Numbers are "+Arrays.toString(myNumbers));
         
         
-        for(int number : myNumbers){
-        
-            if(number>maxNumber){
-            
-                maxNumber=number;
-            }
-        }
-        for(int number : myNumbers){
-        
-            if(number<minNumber){
-            
-                minNumber=number;
-            }
-        }
-        System.out.println("Max Number Is : "+maxNumber);
-        System.out.println("Min Number Is : "+minNumber);
+//        System.out.println("Enter Five Numbers");
+//        int n=s.nextInt();
+//        int[] myNumbers=new int[n];
+//        
+//        int maxNumber=0;
+//        int minNumber=999999999;
+//        
+//        for(int index=0;index<n;index++){
+//        
+//            System.out.println("Input "+(index+1));
+//            int userInput=s.nextInt();
+//            myNumbers[index]=userInput;
+//        }
+//        System.out.println("My Numbers are "+Arrays.toString(myNumbers));
+//        
+//        
+//        for(int number : myNumbers){
+//        
+//            if(number>maxNumber){
+//            
+//                maxNumber=number;
+//            }
+//        }
+//        for(int number : myNumbers){
+//        
+//            if(number<minNumber){
+//            
+//                minNumber=number;
+//            }
+//        }
+//        System.out.println("Max Number Is : "+maxNumber);
+//        System.out.println("Min Number Is : "+minNumber);
         
         
          // 4.
         
-//        System.out.println("Enter Price Of This Product ");
-//        float n=s.nextFloat();
-//        
-//        if(n>200){
-//        
-//            System.out.println("Discount Amount is "+(200*0.05)+"\n"+" Actual Price is "+(n-(200*0.05)));
-//        }
-//        else{
-//        
-//            System.out.println("Discount Amount is "+(200*0)+"\n"+" Actual Price is "+(n-(200*0.05)));
-//        }
+        System.out.println("Enter Price Of This Product ");
+        float n=s.nextFloat();
+        
+        if(n>200){
+        
+            System.out.println("Discount Amount is "+(n*0.05)+"\n"+" Actual Price is "+(n-(n*0.05)));
+        }
+        else{
+        
+            System.out.println("Discount Amount is "+(n*0)+"\n"+" Actual Price is "+n);
+        }
         
 
 
@@ -75,6 +125,41 @@ public class ClassTestDate21April {
     // 5.
     
     
+//     System.out.println("Enter Five Numbers");
+//        int n=s.nextInt();
+//        int[] myNumbers=new int[n];
+//        
+//       
+//        
+//        for(int index=0;index<n;index++){
+//        
+//            System.out.println("Input "+(index+1));
+//            int userInput=s.nextInt();
+//            myNumbers[index]=userInput;
+//        }
+//        System.out.println("My Numbers are Before Sorting "+Arrays.toString(myNumbers));
+//        
+//        for(int i=0;i<myNumbers.length;i++){
+//        
+//            for(int j=0;j<myNumbers.length-i-1;j++){
+//            
+//                if(myNumbers[j]>myNumbers[j+1]){
+//                
+//                    int temp=myNumbers[j];
+//                    myNumbers[j]=myNumbers[j+1];
+//                    myNumbers[j+1]=temp;
+//                    
+//                }
+//            }
+//        }
+//        System.out.println("My Numbers are After Sorting "+Arrays.toString(myNumbers));
+        
+        
+       
+        
+        
+        
+        
         
     }
     
