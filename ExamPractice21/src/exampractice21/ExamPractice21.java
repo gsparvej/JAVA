0bcb706a00409b3cd5a892ffbcef8411718d7c96
gsparvej <parvejmd4469@gmail.com>
2025-04-21 21:16:54 +0600

    changed

--- ExamPractice21/src/exampractice21/ExamPractice21.java
+++ ExamPractice21/src/exampractice21/ExamPractice21.java
@@ -1,13 +1,49 @@
 
 package exampractice21;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class ExamPractice21 {
 
     
     public static void main(String[] args) {
         
+        // bubble sort start........ 1
         
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Your Array Size for Sorting");
+        int n=s.nextInt();
+        int[] myNumbers=new int[n];
+        
+        for(int index=0;index<n;index++){
+        
+            System.out.println("Enter "+(index+1));
+            int userInput=s.nextInt();
+            myNumbers[index]=userInput;
+            
+            
+        }
+        System.out.println("Before Sorting : "+Arrays.toString(myNumbers));
+        
+        
+        for(int i=0;i<myNumbers.length;i++){
+        
+            for(int j=0;j<myNumbers.length-i-1;j++){
+            
+                if(myNumbers[j]>myNumbers[j+1]){
+                
+                    int temp=myNumbers[j];
+                    myNumbers[j]=myNumbers[j+1];
+                    myNumbers[j+1]=temp;
+                    
+                }
+            
+            }
+        
+        }
+        System.out.println("After Sorting : "+Arrays.toString(myNumbers));
     }
     
 }
