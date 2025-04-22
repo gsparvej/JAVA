0a86caf29acd3cbd121a7e49fe60570ae0630464
gsparvej <parvejmd4469@gmail.com>
2025-04-22 20:08:57 +0600

    add

--- ExamPractice21/src/exampractice21/ExamPractice21.java
+++ ExamPractice21/src/exampractice21/ExamPractice21.java
@@ -15,37 +15,37 @@ public class ExamPractice21 {
         Scanner s=new Scanner(System.in);
         
         
-//        System.out.println("Enter Your Array Size for Sorting");
-//        int n=s.nextInt();
-//        int[] myNumbers=new int[n];
-//        
-//        for(int index=0;index<n;index++){
-//        
-//            System.out.println("Enter "+(index+1));
-//            int userInput=s.nextInt();
-//            myNumbers[index]=userInput;
-//            
-//            
-//        }
-//        System.out.println("Before Sorting : "+Arrays.toString(myNumbers));
-//        
-//        
-//        for(int i=0;i<myNumbers.length;i++){
-//        
-//            for(int j=0;j<myNumbers.length-i-1;j++){
-//            
-//                if(myNumbers[j]>myNumbers[j+1]){
-//                
-//                    int temp=myNumbers[j];
-//                    myNumbers[j]=myNumbers[j+1];
-//                    myNumbers[j+1]=temp;
-//                    
-//                }
-//            
-//            }
-//        
-//        }
-//        System.out.println("After Sorting : "+Arrays.toString(myNumbers));
+        System.out.println("Enter Your Array Size for Sorting");
+        int n=s.nextInt();
+        int[] myNumbers=new int[n];
+        
+        for(int index=0;index<n;index++){
+        
+            System.out.println("Enter "+(index+1));
+            int userInput=s.nextInt();
+            myNumbers[index]=userInput;
+            
+            
+        }
+        System.out.println("Before Sorting : "+Arrays.toString(myNumbers));
+        
+        
+        for(int i=0;i<myNumbers.length;i++){
+        
+            for(int j=0;j<myNumbers.length-i-1;j++){
+            
+                if(myNumbers[j]>myNumbers[j+1]){
+                
+                    int temp=myNumbers[j];
+                    myNumbers[j]=myNumbers[j+1];
+                    myNumbers[j+1]=temp;
+                    
+                }
+            
+            }
+        
+        }
+        System.out.println("After Sorting : "+Arrays.toString(myNumbers));
         
         
         // discount start........ 2
