6004487d45fec9c421bd44e547607a6421df1ef4
gsparvej <parvejmd4469@gmail.com>
2025-04-23 16:56:23 +0600

    add

---
+++ LearnBigInteger/src/learnbiginteger/LearnBigInteger.java
@@ -0,0 +1,25 @@
+
+package learnbiginteger;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+public class LearnBigInteger {
+
+    
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+       
+        
+        
+        BigInteger bi=new BigInteger("50");
+        BigInteger bi1=new BigInteger("100");
+        
+        System.out.println(""+bi.add(bi1));
+        
+        
+        
+    }
+    
+}
