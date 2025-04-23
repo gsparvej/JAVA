dcb87537a683edf4f1ce63acaefdb48e46ef02b6
gsparvej <parvejmd4469@gmail.com>
2025-04-23 18:48:34 +0600

    add

--- LearnInheritance/src/learninheritance/country/Finland.java
+++ LearnInheritance/src/learninheritance/country/Finland.java
@@ -5,5 +5,64 @@ package learninheritance.country;
 public class Finland {
     
     
+    private String fincity;
+    private double finarea;
+    private double finpopulation;
+    private String finjob;
+    private double finsalary;
+
+    public Finland() {
+    }
+
+    public Finland(String fincity, double finarea, double finpopulation, String finjob, double finsalary) {
+        this.fincity = fincity;
+        this.finarea = finarea;
+        this.finpopulation = finpopulation;
+        this.finjob = finjob;
+        this.finsalary = finsalary;
+    }
+
+    public String getFincity() {
+        return fincity;
+    }
+
+    public void setFincity(String fincity) {
+        this.fincity = fincity;
+    }
+
+    public double getFinarea() {
+        return finarea;
+    }
+
+    public void setFinarea(double finarea) {
+        this.finarea = finarea;
+    }
+
+    public double getFinpopulation() {
+        return finpopulation;
+    }
+
+    public void setFinpopulation(double finpopulation) {
+        this.finpopulation = finpopulation;
+    }
+
+    public String getFinjob() {
+        return finjob;
+    }
+
+    public void setFinjob(String finjob) {
+        this.finjob = finjob;
+    }
+
+    public double getFinsalary() {
+        return finsalary;
+    }
+
+    public void setFinsalary(double finsalary) {
+        this.finsalary = finsalary;
+    }
+    
+    
+    
     
 }
--- LearnInheritance/src/learninheritance/country/Japan.java
+++ LearnInheritance/src/learninheritance/country/Japan.java
@@ -4,6 +4,63 @@ package learninheritance.country;
 
 public class Japan {
     
+    private String city;
+    private double area;
+    private double population;
+    private String job;
+    private double salary;
+
+    public Japan() {
+    }
+
+    public Japan(String city, double area, double population, String job, double salary) {
+        this.city = city;
+        this.area = area;
+        this.population = population;
+        this.job = job;
+        this.salary = salary;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(double population) {
+        this.population = population;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
     
     
 }
