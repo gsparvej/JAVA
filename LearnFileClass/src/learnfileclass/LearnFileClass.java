b0eb5f28a685b5c2720c84f176d0983fe218604a
gsparvej <parvejmd4469@gmail.com>
2025-04-27 17:27:49 +0600

    add

---
+++ LearnFileClass/src/learnfileclass/LearnFileClass.java
@@ -0,0 +1,40 @@
+package learnfileclass;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LearnFileClass {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i++) {
+
+            try {
+                PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Documents\\parvej"+ i +".txt");
+                pw.print("Hello parvej. Nice to meet you ! ");
+                System.out.println("File Created Successfully ! ");
+                pw.close();
+
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(LearnFileClass.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+//        try {
+//            PrintWriter pw=new PrintWriter(f);
+//            pw.print("Hello parvej. Nice to meet you ! ");
+//            System.out.println("File Created Successfully ! ");
+//            pw.close();
+//            
+//        } catch (FileNotFoundException ex) {
+//            System.out.println("Wrong Location !");
+//           // Logger.getLogger(LearnFileClass.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+
+}
