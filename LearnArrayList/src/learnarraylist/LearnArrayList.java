c79310f176105cfbdd42c4fd1c6dc5b1bf724e11
gsparvej <parvejmd4469@gmail.com>
2025-04-27 09:41:42 +0600

    update

--- LearnArrayList/src/learnarraylist/LearnArrayList.java
+++ LearnArrayList/src/learnarraylist/LearnArrayList.java
@@ -12,10 +12,14 @@ public class LearnArrayList {
         
         ArrayList<StudentArray> studentsList=new ArrayList<>();
         
+        StudentArray s=new StudentArray("Eiasin", "eiasin@gmail.com", 1287745, "+880179-674523");
+        
         studentsList.add(new StudentArray("Md. Parvej Hossen", "parvej@gmail.com", 1287799, "+8801906-600275"));
-        studentsList.add(new StudentArray("Eiasin", "eiasin@gmail.com", 1287745, "+880179-674523"));
+        studentsList.add(s);
         studentsList.add(new StudentArray("Rakib", "rakib@gmail.com", 1286644, "+880153-907856"));
         
+        
+     //  studentsList.remove(s);
         
         System.out.println(""+studentsList);
         
