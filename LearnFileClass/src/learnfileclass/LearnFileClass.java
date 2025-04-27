e600cbf51d46414d2b1f2eb43a37007025625755
gsparvej <parvejmd4469@gmail.com>
2025-04-27 17:40:32 +0600

    add

--- LearnFileClass/src/learnfileclass/LearnFileClass.java
+++ LearnFileClass/src/learnfileclass/LearnFileClass.java
@@ -3,6 +3,7 @@ package learnfileclass;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,11 +11,16 @@ public class LearnFileClass {
 
     public static void main(String[] args) {
 
-
-        for (int i = 1; i <=5; i++) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter Limit ");
+        int limit=s.nextInt();
+        for (int i = 1; i <=limit; i++) {
 
             try {
-                PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Documents\\rakib"+ i +".txt");
+                System.out.println("Enter file name ");
+                String fileName=s.next();
+                PrintWriter pw = new PrintWriter("C:\\Users\\Admin\\Documents\\" + fileName+i+".txt");
+               
                 pw.print("Hello parvej. Nice to meet you ! ");
                 System.out.println("File Created Successfully ! ");
                 pw.close();
