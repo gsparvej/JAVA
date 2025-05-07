173e028ff0c3e289c1bb1dc678f2fa9f3e945b4a
gsparvej <parvejmd4469@gmail.com>
2025-05-07 17:53:46 +0600

    add

--- LearnSorting/src/learnsorting/LearnSorting.java
+++ LearnSorting/src/learnsorting/LearnSorting.java
@@ -17,17 +17,17 @@ public class LearnSorting {
     public static void insertionSort(int[] list) {
 
          int []numbers={19,45,20,80,70,90};
-        for (int i = 1; i < list.length; i++) {
+        for (int i = 1; i < numbers.length; i++) {
 
-            int currentElement = list[i];
+            int currentElement = numbers[i];
 
             int k;
-            for( k=i-1;k>=0 && list[k]> currentElement;k--){
+            for( k=i-1;k>=0 && numbers[k]> currentElement;k--){
             
-            list[k+1]=list[k];
+            numbers[k+1]=numbers[k];
             
             }
-            list[k+1]=currentElement;
+            numbers[k+1]=currentElement;
 
         }
         System.out.println(Arrays.toString(numbers));
