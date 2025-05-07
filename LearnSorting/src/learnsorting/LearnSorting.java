92c061b7f9ca758c7fa60eed26c768dcb70094a3
gsparvej <parvejmd4469@gmail.com>
2025-05-07 17:50:27 +0600

    add

--- LearnSorting/src/learnsorting/LearnSorting.java
+++ LearnSorting/src/learnsorting/LearnSorting.java
@@ -1,14 +1,39 @@
-
 package learnsorting;
 
+import java.util.Arrays;
 
 public class LearnSorting {
 
-    
     public static void main(String[] args) {
         
+        int []numbers={19,45,20,80,70,90};
+
+        insertionSort(numbers);
         
+        // sir er class dekhe ekhane correction korte hbe *******************
         
     }
-    
+
+    public static void insertionSort(int[] list) {
+
+         int []numbers={19,45,20,80,70,90};
+        for (int i = 1; i < list.length; i++) {
+
+            int currentElement = list[i];
+
+            int k;
+            for( k=i-1;k>=0 && list[k]> currentElement;k--){
+            
+            list[k+1]=list[k];
+            
+            }
+            list[k+1]=currentElement;
+
+        }
+        System.out.println(Arrays.toString(numbers));
+        
+        
+
+    }
+
 }
