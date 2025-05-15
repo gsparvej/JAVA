2d49bd3caf08c6a58e74e6d0ccb5daa604155e96
gsparvej <parvejmd4469@gmail.com>
2025-05-15 23:21:56 +0600

    add

--- LearnDatabaseConnect/src/learndatabaseconnect/LearnDatabaseConnect.java
+++ LearnDatabaseConnect/src/learndatabaseconnect/LearnDatabaseConnect.java
@@ -1,13 +1,20 @@
 
 package learndatabaseconnect;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class LearnDatabaseConnect {
 
    
     public static void main(String[] args) {
         
-        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(LearnDatabaseConnect.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
