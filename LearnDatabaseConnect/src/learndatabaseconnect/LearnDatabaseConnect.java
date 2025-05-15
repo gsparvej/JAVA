056107fbe4cca87bcfe8cc759514dbafbc4eabcd
gsparvej <parvejmd4469@gmail.com>
2025-05-16 01:13:17 +0600

    add

--- LearnDatabaseConnect/src/learndatabaseconnect/LearnDatabaseConnect.java
+++ LearnDatabaseConnect/src/learndatabaseconnect/LearnDatabaseConnect.java
@@ -1,8 +1,12 @@
 
 package learndatabaseconnect;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import learndatabaseconnect.database.DbUtil;
+
 
 
 public class LearnDatabaseConnect {
@@ -10,9 +14,23 @@ public class LearnDatabaseConnect {
    
     public static void main(String[] args) {
         
+        DbUtil db=new DbUtil();
+        
+        String insertSql="insert into student(name, email, address)"+"values(?,?,?)";
+      
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
+            PreparedStatement ps=db.getCon().prepareStatement(insertSql);
+            
+            ps.setString(1, "Md Rakib Islam");
+            ps.setString(2, "rakib@gmail.com");
+            ps.setString(3, "Dhaka");
+            
+            ps.executeUpdate();
+            ps.close();
+            db.getCon().close();
+            System.out.println("done ");
+            
+        } catch (SQLException ex) {
             Logger.getLogger(LearnDatabaseConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
