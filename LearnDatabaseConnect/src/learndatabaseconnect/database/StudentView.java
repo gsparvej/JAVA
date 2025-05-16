74e9db82fdb820e23a454a89a7f1efdee282bcf2
gsparvej <parvejmd4469@gmail.com>
2025-05-16 08:25:36 +0600

    add

--- LearnDatabaseConnect/src/learndatabaseconnect/database/StudentView.java
+++ LearnDatabaseConnect/src/learndatabaseconnect/database/StudentView.java
@@ -8,12 +8,12 @@ package learndatabaseconnect.database;
  *
  * @author MY COMPUTER
  */
-public class Student extends javax.swing.JFrame {
+public class StudentView extends javax.swing.JFrame {
 
     /**
      * Creates new form Student
      */
-    public Student() {
+    public StudentView() {
         initComponents();
     }
 
@@ -74,6 +74,7 @@ public class Student extends javax.swing.JFrame {
         getContentPane().add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, -1, 230));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -93,20 +94,21 @@ public class Student extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Student.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StudentView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Student.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StudentView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Student.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StudentView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Student.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StudentView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Student().setVisible(true);
+                new StudentView().setVisible(true);
             }
         });
     }
